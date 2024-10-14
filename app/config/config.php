b934fb24dff8cea738b4798b87e6de20efdfae77
pochun840 <0840@kilews.com.tw>
2024-10-14 14:31:24 +0800

    update

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,4 @@
 <?php
-
-
 // App 根目錄，這是引入 app 資料夾裡的資源用的
 define('APPROOT', dirname(dirname(__FILE__)) . '/');
 
@@ -9,5 +7,9 @@ define('URLROOT', '../public/'); //local用
 
 // 網站名稱
 define('SITENAME', 'iAMS');
+
+// GTCS控制器FTP帳密
+define('FTP_USER', 'kls');
+define('FTP_PASSWORD', '12345678rd');
 
 //define('CONTROLLER_IP', '192.168.0.186');
